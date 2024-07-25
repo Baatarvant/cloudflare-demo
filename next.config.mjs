71c876e91f93289148b28f2200a3626522af1057
Baatarvant <baatarvant@gmail.com>
2024-07-25 15:38:54 +0800

    pages ci

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
-import { paraglide } from "@inlang/paraglide-js-adapter-next/plugin";
 
 // Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
 // (when running the application with `next dev`), for more information see:
@@ -8,15 +7,6 @@ if (process.env.NODE_ENV === "development") {
   await setupDevPlatform();
 }
 
-const nextConfig = {
-  output: "export",
-  distDir: "out",
-};
+const nextConfig = {};
 
-export default paraglide({
-  paraglide: {
-    project: "./project.inlang",
-    outdir: "./src/paraglide",
-  },
-  ...nextConfig,
-});
+export default nextConfig;
